85909bbc6bb18f0aca4a62e5295c091cf76ff026
khaitbek <khaitbek2005@gmail.com>
2022-06-23 03:53:48 +0500

    Optimize rendering books

--- script/app.js
+++ script/app.js
@@ -1,48 +1,40 @@
 let books_list = document.querySelector(".books-list");
 let books_bottom = document.querySelector(".books-bottom");
+let search_input = document.getElementById("search");
+let books_container = document.querySelector("[data-book-template]");
+let users = []
 
 let displayMenuItems = (menuItems) => {
-    let displayMenu = menuItems.map(item => {
-        let parsed_price = Math.floor(parseFloat(item.price.replace("$", "")));
-        return `<li class="books-item">
-                  <div class="item-top">
-                    <p class="sale">Sale</p>
-                    <img
-                      class="item-img"
-                      src="${item.image}"
-                      alt="Template image"
-                      width="160"
-                      height="160"
-                    />
-                    <div class="item-wrapper">
-                      <button class="bucket-btn item-wrapper-btn">
-                        <i class="fa-solid fa-basket-shopping"></i>
-                      </button>
-                      <button class="like-btn item-wrapper-btn">
-                        <i class="fa-solid fa-heart"></i>
-                      </button>
-                    </div>
-                  </div>
-                  <h3 class="book-title">
-                    <a class="book-link" href="#">${item.title}</a>
-                  </h3>
-                  <del class="book-del">${parsed_price > 0 ? parsed_price - 10 + '$' : ""}</del
-                  ><ins class="book-price">${item.price}</ins>
-                  <p class="book-price-rent">0 soums / 12 months</p>
-                  <button class="buy-btn" href="#">Buy now</button>
-                </li>`;
+    users = menuItems.map(item => {
+       let parsed_price = Math.floor(parseFloat(item.price.replace("$", "")));
+       card = books_container.content.cloneNode(true).children[0];
+       let img = card.querySelector("[data-book-img]");
+       img.setAttribute("src",item.image);
+       let name = card.querySelector("[data-book-name]")
+       name.textContent = item.title
+       let old_price = card.querySelector("[data-book-old-price]")
+       old_price.textContent = parsed_price > 0 ? `${parsed_price + 10}$` : ""
+       let price = card.querySelector("[data-book-price]")
+       price.textContent = `${parsed_price}$`
+       let rent_price = card.querySelector("[data-book-rent-price]")
+       rent_price.textContent = parsed_price > 0 ? `${Math.floor(parsed_price / 12)}$ / 12 months` : ""
+       books_list.append(card)
+
+      //  return the user
+      return {name:item.name, img:item.img, title:item.title, price:item.price, subtitle:item.subtitle}
     })
-    displayMenu = displayMenu.join("")
-    displayMenu = new DOMParser().parseFromString(displayMenu, "text/html")
-    let items = displayMenu.querySelectorAll("li");
-    for (let item of items) {
-        books_list.appendChild(item)
-    }
 }
+
+// search bar
+search_input.addEventListener("input",(e)=>{
+  let text = e.currentTarget.value;
+
+})
 
 fetch("https://api.itbook.store/1.0/search/mongodb")
   .then((res) => res.json())
   .then((data) => {
+    // console.log(data.books);
     displayMenuItems(data.books)
     
   });
