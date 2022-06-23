d390db3fadab215d10244a87a313eb3d48e7ca7e
khaitbek <khaitbek2005@gmail.com>
2022-06-23 19:30:27 +0500

    Add filter by price

--- script/app.js
+++ script/app.js
@@ -3,6 +3,20 @@ let books_bottom = document.querySelector(".books-bottom");
 let search_input = document.getElementById("search");
 let books_container = document.querySelector("[data-book-template]");
 let books = []
+let filter_books = document.querySelector("select#filter_books");
+filter_books.addEventListener("change",(e)=>{
+  let selected_value = e.currentTarget.value;
+  if(selected_value === "all"){
+    books.forEach(book =>{
+      book.element.classList.remove("hide")
+    })
+    return
+  }
+  books.forEach(book =>{
+    let isFiltered = book.price_type === selected_value;
+    book.element.classList.toggle("hide",!isFiltered)
+  })
+})
 
 let displayMenuItems = (menuItems) => {
     books = menuItems.map(item => {
@@ -19,8 +33,9 @@ let displayMenuItems = (menuItems) => {
        let rent_price = card.querySelector("[data-book-rent-price]")
        rent_price.textContent = parsed_price > 0 ? `${Math.floor(parsed_price / 12)}$ / 12 months` : ""
        books_list.append(card)
-      //  return the user
-      return {name:item.name, img:item.img, title:item.title, price:parsed_price, subtitle:item.subtitle,element:card}
+      //  return the book
+      
+      return {name:item.name, img:item.img, title:item.title, price:parsed_price, subtitle:item.subtitle,element:card,price_type:parsed_price < 30 ? "low_price" : "high_price"}
     })
 }
 
@@ -37,7 +52,6 @@ search_input.addEventListener("input",(e)=>{
 fetch("https://api.itbook.store/1.0/search/mongodb")
   .then((res) => res.json())
   .then((data) => {
-    console.log(data.books);
     displayMenuItems(data.books)
     
   });
