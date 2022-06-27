e0a60818f9ae54c9d4a5c1e7a814c9a4884b25b2
khaitbek <khaitbek2005@gmail.com>
2022-06-27 06:25:17 +0500

    Register section is done but still needs some little fixing

--- script/register.js
+++ script/register.js
@@ -1,17 +1,36 @@
 const form = document.querySelector(".register-form");
+const users = []
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const inputs = form.querySelectorAll("input");
-    inputs.forEach(input =>{
-
-    })  
+    e.preventDefault()
+    let user = {}
+    let name = form.querySelector("#name");
+    let username = form.querySelector("#username");
+    let email = form.querySelector("#email");
+    let password = form.querySelector("#password");
+    if (validate_username(name.value) && validate_username(username.value) && validate_email(email.value) && validate_password(password.value)) {
+        user.name_ = name.value
+        user.username = username.value
+        user.email = email.value
+        user.password = password.value
+        window.location = "../cabinet.html"
+    } else {
+        alert("something went wrong");
+    }
 })
 
-function validate_username(username){
+function validate_username(username) {
     return username.length >= 6
 }
 
-function validate_email(email){
+function validate_email(email) {
     return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(String(email).toLocaleLowerCase())
+}
+
+function validate_password(password) {
+    return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/.test(password)
+    // should contain at least one digit
+    //   (?=.*[a-z])       // should contain at least one lower case
+    //   (?=.*[A-Z])       // should contain at least one upper case
+    //   [a-zA-Z0-9]{8,}   // should contain at least 8 from the mentioned characters
 }
