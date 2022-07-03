2a8b1c128c4fb479edc3e5a824734b9b3e5c26ce
khaitbek <khaitbek2005@gmail.com>
2022-07-03 12:49:12 +0500

    Huge changes in styling

--- src/public/script/index.js
+++ src/public/script/index.js
@@ -1,33 +1,37 @@
-let modal = document.querySelector(".modal");
-let form = document.querySelector(".modal-form");
-let location_select = document.querySelector(".location_select");
-let modalBtn = document.querySelector(".modal-btn");
-let post_btn = document.querySelector(".post-btn")
-let cabinet_btn = document.querySelector(".cabinet-btn")
-let user = {}
+// let modal = document.querySelector(".modal");
+// let form = document.querySelector(".modal-form");
+// let location_select = document.querySelector(".location_select");
+// let modalBtn = document.querySelector(".modal-btn");
+// let post_btn = document.querySelector(".post-btn")
+// let user = {}
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    if (location_select.value === "O'z hududingizni tanlang") return
-})
+// form.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     if (location_select.value === "O'z hududingizni tanlang") return
+// })
 
-if (!modal.style.display === "none") {
-    document.body.style.overflow = "visible"
+// if (!modal.style.display === "none") {
+//     document.body.style.overflow = "visible"
+// }
+
+// location_select.addEventListener("change", () => {
+//     modalBtn.classList.remove("not-allowed")
+// })
+
+// modalBtn.addEventListener("click", () => {
+//     if (location_select.value === "O'z hududingizni tanlang") return
+//     user.location = location_select.value
+//     modal.style.display = "none"
+//     document.body.style.overflow = "visible"
+//     console.log(user);
+// })
+
+function navigateToCabinet() {
+    let cabinet_btn = document.querySelector(".cabinet-btn")
+    cabinet_btn.addEventListener("click", () => {
+        location.href = "./views/register.html"
+    })
 }
 
-location_select.addEventListener("change", () => {
-    modalBtn.classList.remove("not-allowed")
-})
-
-modalBtn.addEventListener("click", () => {
-    if (location_select.value === "O'z hududingizni tanlang") return
-    user.location = location_select.value
-    modal.style.display = "none"
-    document.body.style.overflow = "visible"
-    console.log(user);
-})
-
-cabinet_btn.addEventListener("click", () => {
-    location.href = "./views/register.html"
-})
+navigateToCabinet();
 
--- src/public/script/render.js
+++ src/public/script/render.js
@@ -1,55 +1,45 @@
+import displayMenuItems from './app'
+
 let books_list = document.querySelector(".books-list");
 let books_container = document.querySelector("[data-book-template]");
 let categories = document.querySelector("[data-category-template]");
 let books = [];
 
-let displayMenuItems = (menuItems) => {
-  books = menuItems.map((item) => {
-    let parsed_price = Math.floor(parseFloat(item.price));
-    console.log(item);
-    let card = books_container.content.cloneNode(true).children[0];
+// let displayMenuItems = (menuItems) => {
+//   books = menuItems.map((item) => {
+//     let parsed_price = Math.floor(parseFloat(item.price));
+//     let card = books_container.content.cloneNode(true).children[0];
+//     let item_top = card.querySelector(".item-top");
+//     item_top.append(item.image);
+//     item.image.width = 160
+//     item.image.height = 160
+//     item.image.classList.add("item-img")
+//     let name = card.querySelector("[data-book-name]");
+//     name.textContent = item.name;
+//     let desc = card.querySelector(".book-desc");
+//     desc.textContent = item.subtitle
+//     let old_price = card.querySelector("[data-book-old-price]");
+//     old_price.textContent = parsed_price > 0 ? `${parsed_price + 10}$` : "";
+//     let price = card.querySelector("[data-book-price]");
+//     price.textContent = `${parsed_price}$`;
+//     let rent_price = card.querySelector("[data-book-rent-price]");
+//     rent_price.textContent =
+//       parsed_price > 0 ? `${Math.floor(parsed_price / 12)}$ / 12 months` : "";
+//     books_list.append(card);
+//     //  return the book
 
-    let item_top = card.querySelector(".item-top");
-    item_top.append(item.image);
-    item.image.width = 160
-    item.image.height = 160
-    item.image.classList.add("item-img")
-    let name = card.querySelector("[data-book-name]");
-    name.textContent = item.name;
-    let desc = card.querySelector(".book-desc");
-    desc.textContent = item.subtitle
-    console.log(`this needs to be in a p tag ${item.subtitle}`);
-    let old_price = card.querySelector("[data-book-old-price]");
-    old_price.textContent = parsed_price > 0 ? `${parsed_price + 10}$` : "";
-    let price = card.querySelector("[data-book-price]");
-    price.textContent = `${parsed_price}$`;
-    let rent_price = card.querySelector("[data-book-rent-price]");
-    rent_price.textContent =
-      parsed_price > 0 ? `${Math.floor(parsed_price / 12)}$ / 12 months` : "";
-    books_list.append(card);
-    //  return the book
+//     return {
+//       name: item.name,
+//       img: item.img,
+//       title: item.title,
+//       price: parsed_price,
+//       subtitle: item.subtitle,
+//       element: card,
+//       price_type: parsed_price < 30 ? "low_price" : "high_price",
+//     };
+//   });
+// };
 
-    return {
-      name: item.name,
-      img: item.img,
-      title: item.title,
-      price: parsed_price,
-      subtitle: item.subtitle,
-      element: card,
-      price_type: parsed_price < 30 ? "low_price" : "high_price",
-    };
-  });
-};
-
-function getImg(src) {
-  return `<img
-  class="item-img"
-  src="${src}"
-  data-book-img
-  alt="Template image"
-  width="160"
-  height="160"
-/>`
-}
+displayMenuItems();
 
 export default displayMenuItems;
