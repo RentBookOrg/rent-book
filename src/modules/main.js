de7deaf845c7d8a8ad00f778ebf21f8f66070b0e
eugene0928 <umid09tuxtayev.com>
2022-07-08 18:40:08 +0500

    cabinet and register routes are added

--- src/modules/main.js
+++ src/modules/main.js
@@ -2,7 +2,16 @@ import { Router } from "express"
 
 const router = Router()
 
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
     res.render('index')
 })
+
+router.get('/cabinet', (req, res) => {
+    res.render('cabinet')
+})
+
+router.get('/register', (req, res) => {
+    res.render('register')
+})
+
 export default router
