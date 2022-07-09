16b6fc12402e8928dffb5dedc4db655ad56327c4
khaitbek <khaitbek2005@gmail.com>
2022-07-09 20:39:44 +0500

    Fix major bugs

--- src/public/script/location.js
+++ src/public/script/location.js
@@ -1,32 +1,41 @@
-import {getBookLocationsFromDb, getBooksByLocation, login} from './db.js';
+import { getBookLocationsFromDb, getBooksByLocation, login } from './db.js';
 import displayMenuItems from './app.js'
 
 let form = document.querySelector(".modal-form");
 let options = form.querySelectorAll("option");
-let isUserLocated = window.localStorage.getItem("user_location_id")
+let isUserLocated
+if (window.localStorage.getItem("user_location_id")) {
+    isUserLocated = true
+} else {
+    isUserLocated = false
+}
+console.log(isUserLocated);
 let locations
-if(isUserLocated){
-    locations = await getBookLocationsFromDb().then(res => res.json()).then(data =>{
-        return data
-    })
+
+locations = await getBookLocationsFromDb().then(res => res.json()).then(data => {
+    return data
+})
+if (isUserLocated) {
     await getBooksByLocation().then(res => res.json()).then(data => displayMenuItems(data.data[0]))
 }
 
 
 
 
-options.forEach( (option) =>{
-    locations.data.forEach(location =>{
-        if(location.location_name === capitalizeFirstLetter(option.value)){
-            option.setAttribute("data-location_id",location.location_id)
+
+options.forEach((option) => {
+
+    locations.data.forEach(location => {
+        if (location.location_name === capitalizeFirstLetter(option.value)) {
+            option.setAttribute("data-location_id", location.location_id)
             let book_location_id = location.location_id
-            window.localStorage.setItem("user_location_id",book_location_id)
+            window.localStorage.setItem("user_location_id", book_location_id)
         }
     });
-    
+
 })
 
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
-  
+
